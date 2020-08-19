f0d7e831005f7016caef3757b8397b2c4fac631d
ss92426 <69694832+ss92426@users.noreply.github.com>
2020-08-18 17:33:22 -0700

    Add files via upload

---
+++ Gunboat1.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Gunboat1
+{
+	public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in); 
+        System.out.println("Enter distance to target.\n"); 
+        double d = reader.nextDouble(); 
+        System.out.println("Enter the height of the battery relative to the water line.\n");
+        double h = reader.nextDouble();  
+        System.out.println("Enter the initial velocity of the shell.\n");
+        double v = reader.nextDouble(); 
+
+        double a = -4.9*(d/v)*(d/v);
+        double b = d;
+        double c = a + h;
+
+        double tanTheta = (-b + Math.sqrt(b*b - 4*a*c))/(2*a);
+        double angle = Math.atan(tanTheta)*180/Math.PI;
+        System.out.println("The angle is: " + angle);
+
+    }
+}
